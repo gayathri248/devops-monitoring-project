9326e8ac28ed9526c21364e97aa4d611c70df2da
root <root@LAPTOP-P0MR1DK8.localdomain>
2026-07-16 05:35:20 +0000

    Completed DevOps Monitoring Project with Prometheus and Grafana

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, Response
+from prometheus_client import generate_latest, Counter
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter(
+    'app_requests_total',
+    'Total app requests'
+)
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.inc()
+
 @app.route("/")
 def home():
-    return f"""
-    <h1>Welcome to DevOps Monitoring Project</h1>
-    <h2>{datetime.now()}</h2>
-    """
+    return "DevOps Monitoring App Running"
+
+@app.route("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        mimetype="text/plain"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
